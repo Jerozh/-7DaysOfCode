5741c02efea94373057b8267ff169941bc052a44
Jerozh <jeronimoh1990@gmail.com>
2024-08-15 12:22:13 -0300

    Crea el primer desafio de #7daysofcode

---
+++ 1dayofcode.js
@@ -0,0 +1,25 @@
+let numeroUn = 1;
+let stringUn = '1';
+let numeroTreinta = 30;
+let stringTreinta = '30';
+let numeroDiez = 10;
+let stringDiez = '10';
+
+//determina si
+if (numeroUn == stringUn){
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+}else{
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
+}
+
+if (numeroTreinta == stringTreinta){
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+}else{
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
+}
+
+if (numeroDiez == stringDiez){
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+}else{
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
+}
